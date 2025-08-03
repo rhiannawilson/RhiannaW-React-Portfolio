ae86f5241d50e6f0cbb8fb93e4c1fd545bd5e8bb
rmay <159880101+rhiannawilson@users.noreply.github.com>
2025-08-03 23:32:37 +1000

    developed the About page, for readability

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,31 +5,81 @@ import './styles/pages.css';
 import PageWrapper from '../pages/UI/PageWrapper';
 
 function About() {
-    const text = 'About'
     return (
         <PageWrapper>
-    <div className="aboutPage p-5 m-5">
-            <h2 className="display-5 aboutHeading fw-bold">About</h2>
-<p className='aboutParagraph '>Over the last six months, I have had the privilege of studying at the University of Sydney, expanding and developing skills in various programming technologies. In 2020, with a strong focus on frontend development, I mastered HTML5 and CSS3, completing Codecademy course which opened the doors into the world of website development.
-</p> 
-<p>
-Fast forward to January 2024, I enrolled on the a Full Stack Engineer course with EDX Bootcamps and the University of Sydney, and became certified in August of this year. 
-</p>
-<p>
-We delved straight into front-end basics to advanced CSS and different frameworks such as JavaScript, TailwindCSS, Bootstrap, and jQuery, before advancing to React, which became the cornerstone of my frontend expertise. I progressed into the world of backend technology, which equipped me with the ability to work with Node, Express.js, and FastAPI, gaining proficiency in several databases, including MySQL, PostgreSQL, and MongoDB, and became adept at utilising Sequelize and GraphQL to manage data with efficiency and precision. 
-</p>
-<p>
-During this course we used essential tools and platforms such as Visual Studio Code, Git, GitHub, GitLab, and the command-line interface as well as Agile methodologies and dedicated group projects to facilitate scrum project management. I also familiarised myself with CMS platforms like WordPress and deployment services like Render, while expanding my knowledge of Apollo for state management and data fetching in React applications. 
-</p>
-<p>
-Through this comprehensive learning experience, I have developed a versatile skill set that spans both frontend and backend technologies, enabling me to build and manage robust applications. Moreover, I am proficient in operating across different environments, including Windows and MacOS, which allows me to adapt to various development scenarios with ease. 
-</p>
-<p>
-This journey has not only expanded my technical abilities but also reinforced my passion for software development, preparing me to tackle future challenges with confidence and creativity.
- </p>
- </div>
- </PageWrapper>
-  )
-  }
-  export default About;
-  
+            <div className="aboutPage p-5 m-5">
+
+<section>
+<h1 className="display-5 aboutHeading fw-bold">
+<strong>2020</strong>
+</h1>
+                <h3 className='p-5 m-5'>
+                with a strong focus on <strong>frontend development</strong>,
+                I mastered <strong>HTML5</strong> and <strong>CSS3</strong>, completing a{' '}
+                <strong>Codecademy</strong> course which opened the doors into the world of
+                website development. During the pandemic this is where I had the time to explore.
+                </h3>
+                </section>
+<section>
+<h1 className="display-5 aboutHeading fw-bold">
+<strong>2024</strong>
+</h1>
+
+                <h3 className='p-5 m-5'>
+            I had the privilege to study at the <strong>University of Sydney</strong>,
+            expanding and developing skills in various programming technologies.
+            I enrolled in a{' '}
+            <strong>Full Stack Engineer</strong> course with <strong>EDX Bootcamps</strong> and
+            the <strong>University of Sydney</strong>, and became{' '}
+            <strong>certified in August</strong> of this year.
+           </h3>
+
+           </section>
+           
+                <h3 className='p-1 m-5'>
+            We delved straight into <strong>frontend basics</strong> to advanced{' '}
+            <strong>CSS</strong> and frameworks such as <strong>JavaScript</strong>,{' '}
+            <strong>TailwindCSS</strong>, <strong>Bootstrap</strong>, and <strong>jQuery</strong>,
+            before advancing to <strong>React</strong>, which became the cornerstone of my
+            frontend expertise. I progressed into <strong>backend technology</strong>,
+            working with <strong>Node</strong>, <strong>Express.js</strong>, and <strong>FastAPI</strong>,
+            gaining proficiency in databases including <strong>MySQL</strong>,{' '}
+            <strong>PostgreSQL</strong>, and <strong>MongoDB</strong>, and became adept at using{' '}
+            <strong>Sequelize</strong> and <strong>GraphQL</strong> for data management.
+            </h3>
+            
+                <h3 className='p-1 m-5'>
+            During this course, we used tools like < strong > Visual Studio Code</strong >, {' '}
+            < strong > Git</strong >, <strong>GitHub</strong>, <strong>GitLab</strong>, and the{' '}
+            <strong>command-line interface</strong>, while applying{' '}
+            <strong>Agile methodologies</strong> and working on{' '}
+            <strong>group projects</strong> for <strong>scrum project management</strong>.I also
+            gained experience with <strong>WordPress</strong>, deployment services like{' '}
+            <strong>Render</strong>, and expanded my knowledge of < strong > Apollo</strong > for
+            <strong> state management</strong> and < strong > data fetching</strong > in React
+            applications.
+            </h3>
+
+                <h3 className='p-1 m-5'>
+            This journey has not only < strong > expanded my technical abilities</strong > but
+            also < strong > reinforced my passion</strong > for software development,
+            preparing me to tackle future challenges with{' '}
+            < strong > confidence and creativity</strong >.
+            </h3>
+
+
+<section>
+<h1 className="display-5 p-5 m-1 aboutHeading fw-bold">
+<strong>2025</strong>
+</h1>
+                <h3 className='p-5 m-5'>
+                Continuing to update my portfolio, take on new projects and seeking full time employment in Wed Development.
+                </h3>
+                </section>
+
+                </div>
+    </PageWrapper >
+  );
+}
+
+export default About;
