05efe7ede27d22e781941156f36a57364f357105
rmay <159880101+rhiannawilson@users.noreply.github.com>
2025-08-29 19:44:52 +1000

    progressed the about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,13 +7,14 @@ import PageWrapper from '../pages/UI/PageWrapper';
 function About() {
     return (
         <PageWrapper>
-            <div className="aboutPage p-5 m-5">
-
-<section>
+            <div className="p-5 container m-5">
+<div className='row'>
+    
+<section className='col order-first'>
 <h1 className="display-5 aboutHeading fw-bold">
 <strong>2020</strong>
 </h1>
-                <h3 className='p-5 m-5'>
+                <h3 className='p-5 aboutText m-5'>
                 with a strong focus on <strong>frontend development</strong>,
                 I mastered <strong>HTML5</strong> and <strong>CSS3</strong>, completing a{' '}
                 <strong>Codecademy</strong> course which opened the doors into the world of
@@ -25,18 +26,15 @@ function About() {
 <strong>2024</strong>
 </h1>
 
-                <h3 className='p-5 m-5'>
+                <h3 className='p-5 aboutText m-5'>
             I had the privilege to study at the <strong>University of Sydney</strong>,
             expanding and developing skills in various programming technologies.
             I enrolled in a{' '}
             <strong>Full Stack Engineer</strong> course with <strong>EDX Bootcamps</strong> and
             the <strong>University of Sydney</strong>, and became{' '}
             <strong>certified in August</strong> of this year.
-           </h3>
-
-           </section>
-           
-                <h3 className='p-1 m-5'>
+           </h3>           
+                <h3 className='p-1 aboutText m-5'>
             We delved straight into <strong>frontend basics</strong> to advanced{' '}
             <strong>CSS</strong> and frameworks such as <strong>JavaScript</strong>,{' '}
             <strong>TailwindCSS</strong>, <strong>Bootstrap</strong>, and <strong>jQuery</strong>,
@@ -48,7 +46,7 @@ function About() {
             <strong>Sequelize</strong> and <strong>GraphQL</strong> for data management.
             </h3>
             
-                <h3 className='p-1 m-5'>
+                <h3 className='p-1 aboutText m-5'>
             During this course, we used tools like < strong > Visual Studio Code</strong >, {' '}
             < strong > Git</strong >, <strong>GitHub</strong>, <strong>GitLab</strong>, and the{' '}
             <strong>command-line interface</strong>, while applying{' '}
@@ -57,27 +55,24 @@ function About() {
             gained experience with <strong>WordPress</strong>, deployment services like{' '}
             <strong>Render</strong>, and expanded my knowledge of < strong > Apollo</strong > for
             <strong> state management</strong> and < strong > data fetching</strong > in React
-            applications.
-            </h3>
-
-                <h3 className='p-1 m-5'>
-            This journey has not only < strong > expanded my technical abilities</strong > but
+            applications. This journey has not only < strong > expanded my technical abilities</strong > but
             also < strong > reinforced my passion</strong > for software development,
             preparing me to tackle future challenges with{' '}
             < strong > confidence and creativity</strong >.
             </h3>
-
+</section>
 
 <section>
 <h1 className="display-5 p-5 m-1 aboutHeading fw-bold">
 <strong>2025</strong>
 </h1>
-                <h3 className='p-5 m-5'>
-                Continuing to update my portfolio, take on new projects and seeking full time employment in Wed Development.
+                <h3 className='p-5 aboutText m-5'>
+                    Developing new projects and seeking full time employment and support in Wed Development.
                 </h3>
                 </section>
+</div>
 
-                </div>
+                </div> 
     </PageWrapper >
   );
 }
